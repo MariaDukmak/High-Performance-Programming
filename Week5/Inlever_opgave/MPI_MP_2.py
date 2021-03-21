e9ae55142e63e571021ab1c39101c08629bc1a5e
Maria <62339103+XXX-MA@users.noreply.github.com>
2021-03-21 19:49:03 +0100

    Week5

--- Week5/Inlever_opgave/MPI_MP_2.py
+++ Week5/Inlever_opgave/MPI_MP_2.py
@@ -7,11 +7,12 @@ from sys import argv
 import numpy as np
 from mpi4py import MPI
 import concurrent.futures
-from multiprocessing import Pool
+
 
 def zeef_vector(N):
     """
-
+    Deze functie vindt de priem getallen van een lijst door gebruik van Numpy te maken.
+    N: len van de lijst
     """
     k = 2
     zeef = np.full(N, True, dtype=bool)
@@ -23,27 +24,36 @@ def zeef_vector(N):
 
     return np.sum(zeef)
 
+
 def worker_zeef(N):
-    lijst =N[0]
+    """
+    Een functie die de prime getallen per sublist van de workers berekent.
+    Hier wordt gebruik van de lijst, k, de start waarde en de index gemaakt.
+    return: Nothing
+    """
+    N_lijst =N[0]
     k = N[1]
     start = N[2]
     index = N[3]
     div_nummber = start + index+k
     if div_nummber % k == 0:
-        lijst[index] = False
+        N_lijst[index] = False
     else:
-        lijst[index] = True
+        N_lijst[index] = True
+
 
 def zeef(N):
-    lijst = N[0]
+    """
+    Een functie die de primes voor de masterworker berekent
+    """
+    N_lijst = N[0]
     k= N[1]
     end = N[2]
     for x in range(k*2, end+1, k):
-        lijst[x] = False
+        N_lijst[x] = False
 
 
 if __name__ == "__main__":
-    start_time = time.time()
     N = int(argv[1])
     process_in_computer = int(argv[2])
 
@@ -53,36 +63,47 @@ if __name__ == "__main__":
     size = comm.Get_size()  # return number of processes in a communicator **numprocesses**
     # print(f'Process {rank} out of {size}')
 
-    k=2
-    end = (rank*N) + N
-    start = (N//size) * rank
+    # Maak variabelen aan die nodig zijn voor
+    # de goed bereken van k, end en de start voor de while loop
+    start = (N // size) * rank
+    end = (N//size) + start
     N_root = end**0.5
-    list_data = []
-    # processen = []
-    if rank == 0:
-        # start_time = time.time() # start time tracking
-        N_list = np.full((N//size)+1, True, dtype=bool)
-        N_list[0:2] = False
+    k=2
 
+    list_data = []
+
+    #Master worker
+    if rank == 0:
+        start_time = time.time() # start time tracking
+        N_list = np.full((N//size)+1, True, dtype=bool)
+        N_list[0:2] = False # Zet 0 en 1 al op False
+
+        # Maak een lijst met de subdata
         for i in range(k, int(N_list)):
-            list_data.append((N_list, i, end))
+            list_data.append([N_list, i, end])
+
         with concurrent.futures.ProcessPoolExecutor() as executor:
-            executor.map(zeef, N_list)
+            executor.map(zeef, list_data)
 
     else:
-
+        # Maak de True lijst aan
         N_list = np.full((N//size), True, dtype=bool)
 
+        # Bereken de priem nummers met behulp van processen
         while k**2 <= end:
             if k <(start) or (N_list[k]):
-                list_data = [(N_list, k, start, i) for i in range(k, (N//size))]
+                for i in range(k, (N // size)):
+                    list_data.append([N_list, k, start, i])
+
                 with concurrent.futures.ProcessPoolExecutor() as executor:
-                    executor.map(worker_zeef, N_list)
+                    executor.map(worker_zeef, list_data)
             k += 1
 
+    # Gather de resultaten van de multiprocessing en de MPI
     result = np.sum(N_list)
     resultresult = comm.reduce(result, op =MPI.SUM, root=0)
 
+    # Als we klaar zijn, print de resultaten uit!
     if rank == 0:
-        print("Number of primes:", resultresult)
+        print("Number of primes:", result)
         print("Total time: ", time.time() - start_time)
