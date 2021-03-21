91e7906f9bc13cb59563f37d723109ce30546554
Maria <62339103+XXX-MA@users.noreply.github.com>
2021-03-21 14:12:50 +0100

    Just MPI added

--- Week5/mpi_zeef.py
+++ Week5/mpi_zeef.py
@@ -1,67 +1,34 @@
+
+# Using MPI
+
 from mpi4py import MPI
 import time
-from sys import argv
-import numpy as np
-from multiprocessing import Process, Manager, Pool
-#
-# start_time = time.time()
-#
-# comm = MPI.COMM_WORLD
-# rank = comm.Get_rank()
-# size = comm.Get_size()
-#
-# num = (rank * 2) + 1
-# maximum, priem = 1000000, []
-#
-#
-# for i in range(num, maximum, size * 2):
-#     true_prime = True
-#     for div_number in range(2, i):
-#         if i % div_number == 0:
-#             true_prime = False
-#             break
-#     if true_prime:
-#         priem.append(i)
-#
-# results = comm.gather(priem, root=0)
-#
-# # de master worker
-# if rank == 0:
-#     total_time = round(time.time() - start_time)
-#
-#     print('Found prime numbers up to: ' + str(maximum))
-#     print('Total processes: ' + str(size))
-#     print("Algorithm took: {:.5f} second(s)".format(total_time))
-#
-#     totalPrimes = comm.reduce(results, op=MPI.SUM, root=0)
-#     print("The final result", totalPrimes)
 
-def zeef():
-    pass
+start_time = time.time()
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+# print('Process {} out of {}'.format(rank,size))
 
-if __name__ == "__main__":
-    n = int(argv[1])
-    process_num = int(argv[2])
-    root_n = int(n**0.5)
-    zeef = [i for i in range(2,n)]
+start_num = (rank * 2) + 1
+maximum = 1000000
 
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+priem = [] # list with all the prime numbers
 
-    if rank == 0:
-        start = time.time()
+# Loop through the numbers
+for i in range(start_num, maximum, size * 2):
+    true_prime = True
+    for div_number in range(2, i):
+        if i % div_number == 0:
+            true_prime = False
+            break
+    if true_prime:
+        priem.append(i)
 
-    wl = np.array_split(zeef, size)[rank]
+results = comm.gather(priem, root=0)
 
-    wl_b = np.array_split(wl, process_num)
-
-    thread_pool = Pool(processes=process_num)
-    lijst = thread_pool.map(zeef, wl_b)
-
-    totalPrimes = comm.reduce(lijst, op=MPI.SUM, root=0)
-
-    if rank == 0:
-        print("Number of primes:", totalPrimes)
-        print("Total time: ", time.time() - start)
+# The master worker
+if rank == 0:
+    total_time = time.time() - start_time
+    print(f"Algorithm took: {total_time:.5f} second(s)")
